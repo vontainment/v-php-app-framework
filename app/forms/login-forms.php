2d3f5fa7b90896130d7632b885ba53636db150fd
services@vontainment.com <services@vontainment.com>
2023-08-14 20:21:10 -0400

    refactor login

--- app/forms/login-forms.php
+++ app/forms/login-forms.php
@@ -8,27 +8,14 @@
  * Description: A Simple PHP App Framework for Building Secure Apps
  */
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     appLog("POST request received.", 2);
     if (isset($_POST["login"]) && isset($_POST['username']) && isset($_POST['password'])) {
         appLog("Login form POST fields received.", 2);
         $username = sanitizeInput($_POST['username']);
         $password = sanitizeInput($_POST['password']);
 
-        // Get user info
-        $userInfo = getUserInfo($username); // use the sanitized username
-        appLog("User info retrieved for user: " . $username, 2);
-
-        // Combined condition to check username matches and password is verified
-        if ($userInfo && $username === $userInfo['username'] && password_verify($password, $userInfo['password'])) {
-            appLog("Username matches and password verified.", 2);
-            $_SESSION['logged_in'] = true;
-            $_SESSION['username'] = $username;
-            $_SESSION['email'] = $userInfo['email']; // set email to session
-            $_SESSION['admin'] = $userInfo['admin']; // set admin to session
-            $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
-            $_SESSION['timeout'] = time();
-            session_regenerate_id(true);
+        if (userLogin($username, $password)) {
             appLog("Redirecting to /home.", 2);
             header('Location: /home');
             exit();
